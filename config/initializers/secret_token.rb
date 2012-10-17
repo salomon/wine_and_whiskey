9ef0323d5fc014e5ce3f35e700a18c43ee3e4fbf
Ryan Salomon <ryan5231@gmail.com>
2012-10-17 10:45:14 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WineAndWhiskey::Application.config.secret_token = 'f442dd0db8d577f28550b30393526a17e1d623caedaf48b98e8df1e73c01c9896ccdd22e995739545135e3d73ee6037056ffc58736b140ad2968fd8fe6041ba2'
